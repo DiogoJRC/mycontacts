42d7cce59e82be7b7ea657c4e1fa7c66a11a55cb
Diogo Capdeville <diogoj.r.c@hotmail.com>
2024-12-22 21:58:05 -0300

    Criando a tabela de Contatos

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS categories (
   name VARCHAR NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS contacts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL UNIQUE,
+  phone VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
