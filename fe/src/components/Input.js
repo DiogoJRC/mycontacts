e65d72252fe8cf15c16251a63d6cfb19552e1c06
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-01-19 15:41:44 -0300

    [MyContacts Front-end] [Trabalhando com Formulários no React] - Criando os componentes Input e Select

---
+++ fe/src/components/Input.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export default styled.input`
+  width: 100%;
+  height: 52px;
+  padding: 0 16px;
+  border: 2px solid #fff;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  background: #fff;
+  font-size: 16px;
+  outline: none;
+  transition: border-color 0.2s ease-in;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray[200]};
+  }
+
+  &:focus,
+  &:active {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
