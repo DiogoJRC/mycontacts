92fb6e040662999172823664c6a0e28463311b80
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-01-20 21:32:43 -0300

    [MyContacts Front-end] [Trabalhando com Formulários no React] - Pegando os dados e lidando com o submit do form

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 import { Form, ButtonContainer } from "./styles";
@@ -9,24 +9,25 @@ import Button from "../Button";
 import FormGroup from "../FormGroup";
 
 export default function ContactForm({ buttonLabel }) {
-  const [name, setName] = useState("Mateus");
-  const emailInput = useRef(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [category, setCategory] = useState("");
 
-  function handleClick() {
-    console.log(emailInput.current.value);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({ name, email, phone, category });
   }
 
   return (
-    <Form>
-      <button type="button" onClick={handleClick}>
-        Loga emailInput
-      </button>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Input
           type="text"
           placeholder="Nome"
-          onChange={(event) => setName(event.target.value)}
           value={name}
+          onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
@@ -34,29 +35,33 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           type="email"
           placeholder="E-mail"
-          ref={emailInput}
-          defaultValue="mateus@devacademy.com.br"
-          onChange={(event) => console.log(event.target.value)}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </FormGroup>
 
       <FormGroup>
-        <Input type="tel" placeholder="Telefone" />
+        <Input
+          type="tel"
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value={1}>Instagram</option>
-          <option value={1}>Instagram</option>
-          <option value={1}>Instagram</option>
-          <option value={1}>Instagram</option>
+          <option value={2}>Discord</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-        <Button type="button" disabled>
-          {buttonLabel}
-        </Button>
+        <Button type="submit">{buttonLabel}</Button>
       </ButtonContainer>
     </Form>
   );
