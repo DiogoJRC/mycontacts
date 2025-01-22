5e58da561a997d8f4bcb88a3123602bee1d34329
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-01-21 21:56:44 -0300

    [MyContacts Front-end] [Integrando com a API] - Criando o middleware do CORS na API

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 require("express-async-errors");
 
+const cors = require("./app/middlewares/cors");
+const errorHandler = require("./app/middlewares/errorHandler");
 const routes = require("./routes");
 
 const app = express();
 
 // Recebe o body enviado na requisição
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-// Error Handler do Express
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3001, () =>
   console.log("🔥 Server started at http://localhost:3001")
