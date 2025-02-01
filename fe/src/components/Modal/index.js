8a61663676f618d30e220f236d44581934b2e644
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-01 20:46:50 -0300

    [MyContacts Front-end] [Finalizando a Aplicação] - Melhorando o uso dos React Portals

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -1,9 +1,9 @@
-import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 import { Overlay, Container, Footer } from "./styles";
 
 import Button from "../Button";
+import ReactPortal from "../ReactPortal";
 
 export default function Modal({
   danger,
@@ -20,34 +20,35 @@ export default function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container $danger={danger}>
-        <h1>{title}</h1>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container $danger={danger}>
+          <h1>{title}</h1>
 
-        <div className="modal-body">{children}</div>
+          <div className="modal-body">{children}</div>
 
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
-          <Button
-            type="button"
-            danger={danger}
-            isLoading={isLoading}
-            onClick={onConfirm}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById("modal-root"),
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
+            <Button
+              type="button"
+              danger={danger}
+              isLoading={isLoading}
+              onClick={onConfirm}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
