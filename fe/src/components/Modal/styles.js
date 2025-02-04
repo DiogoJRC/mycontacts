9febc422fdbe228a4df35609df81d867e2621e5a
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-04 19:43:13 -0300

    [Patterns e Animações no React] [Animações no React] - Animando a entrada do Modal com keyframes

--- fe/src/components/Modal/styles.js
+++ fe/src/components/Modal/styles.js
@@ -1,4 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
+
+const scaleIn = keyframes`
+  from {
+    transform: scale(0);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
 
 export const Overlay = styled.div`
   position: fixed;
@@ -12,6 +32,7 @@ export const Overlay = styled.div`
   padding: 16px;
   background: rgba(34, 34, 34, 0.6);
   backdrop-filter: blur(3px);
+  animation: ${fadeIn} 0.3s;
 `;
 
 export const Container = styled.div`
@@ -21,6 +42,7 @@ export const Container = styled.div`
   border-radius: 4px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   background-color: #fff;
+  animation: ${scaleIn} 0.3s;
 
   > h1 {
     font-size: 22px;
